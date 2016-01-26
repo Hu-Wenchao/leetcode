3f021e0dac67afb2a46a197fc5787c5b449af3e4
Hu-Wenchao <huwcbill@gmail.com>
2016-01-26 15:55:09 +0800

    72, 76, 79, 81, 212

---
+++ prob72_edit_distance.py
@@ -0,0 +1,46 @@
+"""
+Given two words word1 and word2, find the minimum number 
+of steps required to convert word1 to word2. (each 
+operation is counted as 1 step.)
+
+You have the following 3 operations permitted on a word:
+
+a) Insert a character
+b) Delete a character
+c) Replace a character
+
+Analysis:
+Use f[i][j] to represent the shortest edit distance between 
+word1[0,i) and word2[0, j). Then compare the last character 
+of word1[0,i) and word2[0,j), which are c and d respectively 
+(c == word1[i-1], d == word2[j-1]):
+
+if c == d, then : f[i][j] = f[i-1][j-1]
+
+Otherwise we can use three operations to convert word1 to word2:
+
+(a) if we replaced c with d: f[i][j] = f[i-1][j-1] + 1;
+
+(b) if we added d after c: f[i][j] = f[i][j-1] + 1;
+
+(c) if we deleted c: f[i][j] = f[i-1][j] + 1;
+"""
+
+class Solution(object):
+    def minDistance(self, word1, word2):
+        """
+        :type word1: str
+        :type word2: str
+        :rtype: int
+        """
+        l1, l2 = len(word1)+1, len(word2)+1
+        dp = [[0 for _ in xrange(l2)] for _ in xrange(l1)]
+        for i in xrange(l1):
+            dp[i][0] = i
+        for j in xrange(l2):
+            dp[0][j] = j
+        for i in xrange(1, l1):
+            for j in xrange(1, l2):
+                dp[i][j] = min(dp[i-1][j]+1, dp[i][j-1]+1, \
+                               dp[i-1][j-1]+(word1[i-1]!=word2[j-1]))
+        return dp[-1][-1]
---
+++ prob81_search_in_rotated_sorted_array2.py
@@ -0,0 +1,37 @@
+"""
+Follow up for "Search in Rotated Sorted Array":
+What if duplicates are allowed?
+
+Would this affect the run-time complexity? How and why?
+
+Write a function to determine if a given target is in the array.
+"""
+
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: bool
+        """
+        l, r = 0, len(nums) -1
+        while l <= r:
+            mid = 1 + (r - l)//2
+            if nums[mid] == target:
+                return True
+            while l < mid and nums[l] == nums[mid]:
+                l += 1
+            if nums[l] <= nums[mid]:
+                if nums[l] <= target <= nums[mid]:
+                    r = mid - 1
+                else:
+                    l = mid + 1
+            else:
+                if nums[mid] < target <= nums[r]:
+                    l = mid + 1
+                else:
+                    r = mid - 1
+        return False
+
+        # For python, there is a cheating solution.
+        # return target in nums
