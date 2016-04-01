6997a86cc35951b6f0aca7516e64734593227804
Hu-Wenchao <huwcbill@gmail.com>
2016-04-01 13:52:06 +0800

    0401

---
+++ prob209_Minimum_size_subarray_sum.py
@@ -0,0 +1,50 @@
+"""
+Given an array of n positive integers and a positive integer s, 
+find the minimal length of a subarray of which the sum ≥ s. 
+If there isn't one, return 0 instead.
+
+For example, given the array [2,3,1,2,4,3] and s = 7,
+the subarray [4,3] has the minimal length under the problem constraint.
+"""
+
+class Solution(object):
+    def minSubArrayLen(self, s, nums):
+        """
+        :type s: int
+        :type nums: List[int]
+        :rtype: int
+        """
+        total = left = 0
+        result = len(nums) + 1
+        for right, n in enumerate(nums):
+            total += n
+            while total >= s:
+                result = min(result, right - left + 1)
+                total -= nums[left]
+                left += 1
+        return result if result <= len(nums) else 0
+
+    def minSubArrayLen2(self, s, nums):
+        """
+        :type s: int
+        :type nums: List[int]
+        :rtype: int
+        """
+        result = len(nums) + 1
+        for idx, n in enumerate(nums[1:], 1):
+            nums[idx] = nums[idx - 1] + n
+        left = 0
+        for right, n in enumerate(nums):
+            if n >= s:
+                left = self.find_left(left, right, nums, s, n)
+                result = min(result, right - left + 1)
+        return result if result <= len(nums) else 0
+
+    def find_left(self, left, right, nums, target, n):
+        while left < right:
+            mid = (left + right) // 2
+            if n - nums[mid] >= target:
+                left = mid + 1
+            else:
+                right = mid
+        return left
---
+++ prob221_maximal_square.py
@@ -0,0 +1,32 @@
+"""
+Given a 2D binary matrix filled with 0's and 1's, find the 
+largest square containing all 1's and return its area.
+
+For example, given the following matrix:
+
+1 0 1 0 0
+1 0 1 1 1
+1 1 1 1 1
+1 0 0 1 0
+Return 4.
+"""
+
+class Solution(object):
+    def maximalSquare(self, matrix):
+        """
+        :type matrix: List[List[str]]
+        :rtype: int
+        """
+        if not matrix: return 0
+        m , n = len(matrix),len(matrix[0])
+        dp = [[0 if matrix[i][j]=='0' else 1for j in xrange(n)]
+              for i in xrange(m)]
+
+        for i in xrange(1,m):
+            for j in xrange(1,n):
+                if matrix[i][j] =='1': 
+                    dp[i][j] = min(dp[i-1][j],dp[i][j-1],dp[i-1][j-1])+1
+                else: dp[i][j] = 0
+
+        ans = max([max(i) for i in dp])
+        return ans ** 2
