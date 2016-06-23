42b9225edd046824e6379e686c04e74d2fd52b4f
Hu-Wenchao <huwcbill@gmail.com>
2016-06-23 20:55:41 +0800

    0623

--- prob130_surrounded_regions.py
+++ prob130_surrounded_regions.py
@@ -24,15 +24,15 @@ class Solution(object):
         :type board: List[List[str]]
         :rtype: void Do not return anything, modify board in-place instead.
         """
-        if not any(board): return
-
+        if not any(board):
+            return
         m, n = len(board), len(board[0])
-        save = [ij for k in range(m+n) for ij in ((0, k), (m-1, k), 
-                                                  (k, 0), (k, n-1))]
-        while save:
-            i, j = save.pop()
+        # all edge points
+        edge = [ij for k in xrange(m) for ij in ((k, 0), (k, n-1))]
+        edge += [ij for k in xrange(n) for ij in ((0, k), (m-1, k))]
+        while edge:
+            i, j = edge.pop()
             if 0 <= i < m and 0 <= j < n and board[i][j] == 'O':
                 board[i][j] = 'S'
-                save += (i, j-1), (i, j+1), (i-1, j), (i+1, j)
-
+                edge += (i, j-1), (i, j+1), (i-1, j), (i+1, j)
         board[:] = [['XO'[c == 'S'] for c in row] for row in board]
